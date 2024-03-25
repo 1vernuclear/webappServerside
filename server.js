5877f0439d610404d97b35a722bfb410574747b6
1vernuclear <emil1093@outlook.com>
2024-03-25 09:12:12 +0000

    applied logging

--- server.js
+++ server.js
@@ -2,19 +2,46 @@ const express = require('express');
 require('dotenv').config();
 const logger = require('./utils/logger'); // import logger
 const connectDB = require('./utils/database'); 
+const path = require('path');
+const http = require('http');
+const fs = require('fs');
+const _ = require('lodash');
 
-const app = express();
-const PORT = 3000;
+const server = express();
+server.set('view engine', 'ejs');
+const PORT = process.env.PORT || 3000; 
+
+/**Path to react build, ie. path to static files, 
+ * making them referable from the frontend / browser.
+ * The join results in the path: /opt/webapp/client/dist */
+server.use(express.static(path.join(__dirname, '../client/dist')));
+var pathstatic = path.join(__dirname, '../client/dist');
+
+/**method that logs all requests making it past the nginx server
+meaning all requests with a /api extension (nginx server removes the /api extension, 
+before forwarding the request to the server).*/
+server.use((req, res, next) => {
+  const logInfo = {
+    timestamp: new Date().toISOString(),
+    host: req.hostname,
+    path: req.path,
+    method: req.method,
+  };
+  logger.info('New request made', logInfo);
+  console.log('Static files path', pathstatic); //test log to be removed.
+  next();
+});
+
+server.use((req, res) => {
+  logger.error('404 invoked');
+  res.status(404).render('404');
+}); 
 
 async function startServer() {
   await connectDB(); // Make sure the database is connected before starting the server.
-  app.listen(PORT, () => {
-      logger.info(`Server is running on http://localhost:${PORT}`);
+  server.listen(PORT, () => {
+      logger.info(`Server is running on http://localhost:${PORT}`)
   });
 }
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
 startServer().catch(err => logger.error('Failed to start the server', err));
