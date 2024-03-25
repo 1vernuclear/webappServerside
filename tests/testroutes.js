a0247063f57f276ac8412b36edae29f7986fa553
1vernuclear <emil1093@outlook.com>
2024-03-25 18:19:17 +0100

    added new script to test routes

---
+++ tests/testroutes.js
@@ -0,0 +1,60 @@
+const product = require('../models/testproducts');
+const connectDB = require('../utils/database'); 
+const logger = require('../utils/logger');
+const express = require('express');
+const path = require('path');
+
+const PORT = 3000;
+const server = express();
+
+/**Path to react build, ie. path to static files, 
+ * making them referable from the frontend / browser.
+ * The join results in the path: /opt/webapp/client/dist */
+ server.use(express.static(path.join(__dirname, '../client/dist')));
+ var pathstatic = path.join(__dirname, '../client/dist');
+ 
+/**method that logs all requests making it past the nginx server
+meaning all requests with a /api extension (nginx server removes the /api extension, 
+before forwarding the request to the server).*/
+server.use((req, res, next) => {
+    const logInfo = {
+      timestamp: new Date().toISOString(),
+      host: req.hostname,
+      path: req.path,
+      method: req.method,
+    };
+    logger.info('New request made', logInfo);
+    console.log('Static files path', pathstatic); //test log to be removed.
+    next();
+  });
+
+server.get('/product', (req, res) => {
+    logger.info('in prodcut');
+    product.findById('6601ad24b90e8b93eabc113a')
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        logger.error(err);
+      });
+  });
+  
+  server.get('/all-products', (req, res) => {
+    logger.info('in all products');
+    product.find()
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        logger.error(err);
+      });
+  });
+
+  async function startServer() {
+    await connectDB(); // Make sure the database is connected before starting the server.
+    server.listen(PORT, () => {
+        logger.info(`Server is running on http://localhost:${PORT}`)
+    });
+  }
+  
+  startServer().catch(err => logger.error('Failed to start the server', err));
